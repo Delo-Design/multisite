1eaa2b3b65bbd3c382b2c37cf39755759998b038
Dmitrii Cymbal <tsymbal.mitia@gmail.com>
2021-06-27 00:45:59 +0300

    обновление

--- plugins/multisite/redirectmenus/fields/listsubdomains.php
+++ plugins/multisite/redirectmenus/fields/listsubdomains.php
@@ -9,19 +9,39 @@ class JFormFieldListsubdomains extends JFormFieldSubform
 
 	public function getInput()
 	{
-
 		$subdomains = ConfigHelper::get('subdomains', []);
 		$i          = 0;
-		$values     = [];
-		foreach ($subdomains as $subdomain)
+		$values     = $this->value;
+
+		if (empty($values))
 		{
-			$values['subdomains' . $i] = [
-				'subdomain' => $subdomain->subdomain,
-				'name'      => $subdomain->name,
-				'enable'    => 1,
-			];
-			$i++;
+			foreach ($subdomains as $subdomain)
+			{
+				$values['subdomains' . $i] = [
+					'subdomain' => $subdomain->subdomain,
+					'name'      => $subdomain->name,
+					'enable'    => 1,
+				];
+				$i++;
+			}
 		}
+		else
+		{
+			foreach ($values as &$value)
+			{
+				foreach ($subdomains as $subdomain)
+				{
+
+					if ($subdomain->subdomain === $value['subdomain'])
+					{
+						$value['name'] = $subdomain->name;
+					}
+
+				}
+
+			}
+		}
+
 
 		$this->value = $values;
 
