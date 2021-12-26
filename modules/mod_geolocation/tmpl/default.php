4b47eecbc509d8867d78e3db681c7ed24a026f82
Dmitrii Cymbal <tsymbal.mitia@gmail.com>
2021-12-26 22:07:11 +0300

    обновление

--- modules/mod_geolocation/tmpl/default.php
+++ modules/mod_geolocation/tmpl/default.php
@@ -12,18 +12,37 @@ use Joomla\CMS\Factory;
 
 defined('_JEXEC') or die;
 
-$find = ModGeolocationHelper::findRoute();
-$list = ModGeolocationHelper::getAllSubdomains();
-$active = ModGeolocationHelper::getActive();
-$domain = ModGeolocationHelper::getDomain();
-$config = Factory::getConfig();
-$https = (int)$config->get('force_ssl', 0) === 2 ? 'https://' : 'http://';
+$find            = ModGeolocationHelper::findRoute();
+$list            = ModGeolocationHelper::getAllSubdomains();
+$active          = ModGeolocationHelper::getActive();
+$domain          = ModGeolocationHelper::getDomain();
+$config          = Factory::getConfig();
+$https           = (int) $config->get('force_ssl', 0) === 2 ? 'https://' : 'http://';
+$list_subdomains = $params->get('listsubdomains', []);
+$list_enables    = [];
+foreach ($list_subdomains as $list_subdomain)
+{
+	$list_enables[$list_subdomain->subdomain] = (int) $list_subdomain->enable;
+}
+
+if (
+	isset($list_enables[$active->subdomain]) &&
+	$list_enables[$active->subdomain])
+{
+	return;
+}
+
 ?>
 
 <div class="uk-inline geolocation">
     <i class="location-icon" data-uk-icon="icon: location-full;"></i>
     <span>Ваш город</span>
-	<?php if($find) : ?>
+	<?php
+	if (
+		($find && !isset($list_enables[$find->subdomain])) ||
+		($find && isset($list_enables[$find->subdomain]) && $list_enables[$find->subdomain])
+	) :
+		?>
         <div class="uk-inline">
             <button class="uk-button uk-hidden" type="button"></button>
             <div class="find-regionsorcity" data-uk-dropdown="mode: click">
@@ -31,14 +50,17 @@ $https = (int)$config->get('force_ssl', 0) === 2 ? 'https://' : 'http://';
                 <div class="city"><?php echo $find->name ?></div>
                 <div class="buttons uk-grid-small" data-uk-grid>
                     <div class="uk-width-auto">
-						<?php if((int)$find->default) : ?>
-                            <a href="<?php echo $https ?><?php echo $domain ?>" class="uk-button uk-button-primary uk-width-1-1">Да</a>
+						<?php if ((int) $find->default) : ?>
+                            <a href="<?php echo $https ?><?php echo $domain ?>"
+                               class="uk-button uk-button-primary uk-width-1-1">Да</a>
 						<?php else : ?>
-                            <a href="<?php echo $https ?><?php echo $find->subdomain ?>.<?php echo $domain ?>" class="uk-button uk-button-primary uk-width-1-1">Да</a>
+                            <a href="<?php echo $https ?><?php echo $find->subdomain ?>.<?php echo $domain ?>"
+                               class="uk-button uk-button-primary uk-width-1-1">Да</a>
 						<?php endif; ?>
                     </div>
                     <div class="uk-width-expand">
-                        <button class="uk-button uk-button-only-border button-change-city uk-width-1-1">Выбрать другой</button>
+                        <button class="uk-button uk-button-only-border button-change-city uk-width-1-1">Выбрать другой
+                        </button>
                     </div>
                 </div>
             </div>
@@ -58,14 +80,27 @@ $https = (int)$config->get('force_ssl', 0) === 2 ? 'https://' : 'http://';
         </script>
 	<?php endif; ?>
     <div class="uk-inline">
-        <button class="uk-button uk-button-text" type="button"><?php echo $active->name ?> <i data-uk-icon="icon: chevron-down;ratio:0.8"></i></button>
+        <button class="uk-button uk-button-text" type="button"><?php echo $active->name ?> <i
+                    data-uk-icon="icon: chevron-down;ratio:0.8"></i></button>
         <div class="list-subdomains" data-uk-dropdown="mode: click">
             <ul class="uk-nav uk-dropdown-nav">
 				<?php foreach ($list as $item) : ?>
-                    <?php if((int)$item->default) : ?>
+					<?php
+					if (
+						isset($list_enables[$item->subdomain]) &&
+						$list_enables[$item->subdomain]
+					)
+					{
+						continue;
+					}
+					?>
+
+					<?php if ((int) $item->default) : ?>
                         <li><a href="<?php echo $https ?><?php echo $domain ?>"><?php echo $item->name ?></a></li>
 					<?php else : ?>
-                        <li><a href="<?php echo $https ?><?php echo $item->subdomain ?>.<?php echo $domain ?>"><?php echo $item->name ?></a></li>
+                        <li>
+                            <a href="<?php echo $https ?><?php echo $item->subdomain ?>.<?php echo $domain ?>"><?php echo $item->name ?></a>
+                        </li>
 					<?php endif; ?>
 				<?php endforeach; ?>
             </ul>
